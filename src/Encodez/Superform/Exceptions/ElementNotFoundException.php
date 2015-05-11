b08e1da86a835f890471bd508d2bdeedef5a8541
Muneer Shaheed <muneer@live.it>
2015-05-11 07:12:53 +0400

    initial commit

---
+++ src/Encodez/Superform/Exceptions/ElementNotFoundException.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: muneer
+ * Date: 5/8/15
+ * Time: 4:09 PM
+ */
+
+namespace Encodez\Superform\Exceptions;
+
+use Phalcon\Forms\Exception;
+
+class ElementNotFoundException extends Exception {
+
+}
---
+++ src/Encodez/Superform/Exceptions/InvalidAttributeException.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: muneer
+ * Date: 5/11/15
+ * Time: 6:04 AM
+ */
+
+namespace Encodez\Superform\Exceptions;
+
+
+class InvalidAttributeException extends \Exception {
+
+}
